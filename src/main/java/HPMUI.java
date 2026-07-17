b905ecd35d22b9facb1824bae76fc3cca149bb52
Heimdall <dev@heimdallsec.ch>
2026-07-17 02:58:02 +0200

    Password recognition working.

--- src/main/java/HPMUI.java
+++ src/main/java/HPMUI.java
@@ -1,5 +1,7 @@
 import com.password4j.Password;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
 
 public class HPMUI {
@@ -10,18 +12,38 @@ public class HPMUI {
         frame.setSize(1920, 1080);
         frame.setLayout(new java.awt.FlowLayout());
 
-        // Add the first buttons
+        // Buttons
         JButton generatePWD = new JButton("Generate Password");
-        JTextField output = new JTextField(37);
+        JButton unlockBtn = new JButton("Unlock");
+
+        // Fields
+        JTextField outputPWD = new JTextField(37);
+        JPasswordField passwordField = new JPasswordField(36);
 
         // Add the items to the frame
         frame.add(generatePWD);
-        frame.add(output);
+        frame.add(outputPWD);
+        frame.add(passwordField);
+        frame.add(unlockBtn);
 
-        // Run the code when the button is clicked
+        // Run the password generating code when the button is clicked
         generatePWD.addActionListener(e -> {
             String PWD = PasswordGenerator.generatePassword(16);
-            output.setText(PWD);
+            outputPWD.setText(PWD);
+        });
+
+        // Password unlock code
+        List<PasswordEntry> entries = new ArrayList<>();
+
+        unlockBtn.addActionListener(e -> {
+            String masterPassword = new String(passwordField.getPassword());
+            try {
+                entries.clear();
+                entries.addAll(LoadLogic.load(masterPassword));
+                JOptionPane.showMessageDialog(frame, "Unlocked successfully! " + entries.size() + " entries loaded.");
+            } catch (Exception err) {
+                JOptionPane.showMessageDialog(frame, "Wrong password!");
+            }
         });
 
         frame.setVisible(true);
